17c4d5206b87bbfe6044115f498f9b617f1b9d8f
nero31994 <arenaofvalorph937@gmail.com>
2025-01-21 10:45:41 +0800

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,19 @@
+const playlist = [
+  {
+    name: "CCTV 4",
+    url: "https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_cctv4.mpd",
+    keys: {
+      kid: "b83566836c0d4216b7107bd7b8399366",
+      key: "32d50635bfd05fbf8189a0e3f6c8db09",
+    },
+  },
+  {
+    name: "CNN HD",
+    url: "https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_cnnhd.mpd",
+    keys: {
+      kid: "900c43f0e02742dd854148b7a75abbec",
+      key: "da315cca7f2902b4de23199718ed7e90",
+    },
+  },
+  // Add other channels similarly
+];
